65b4b80e6c07484e3ca969b01fdb9de2b9e5bcc9
Mohammad Hashemi <89422926+Amahseyn@users.noreply.github.com>
2023-11-11 19:13:02 +0330

    add gpu config to MobileNet_Keras.py

--- MobileNet_Keras.py
+++ MobileNet_Keras.py
@@ -7,6 +7,19 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
+# Explicitly set the GPU to use (optional)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 # Define constants
 input_shape = (224, 224, 1)  # Grayscale images have one channel
 num_classes = 1  # Binary segmentation
